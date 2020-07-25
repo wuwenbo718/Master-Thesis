ea95af0c0ce187f910ded2b9463e8cb6f5500ac7
wuwenbo718 <wuwenbo718@win.tu-berlin.de>
2020-07-25 21:38:46 +0200

    update.

--- data_processing.py
+++ data_processing.py
@@ -116,7 +116,12 @@ def compute_HIST(data,bins=9,ranges=(-10,10)):
             feature[i,j*bins:(j+1)*bins] = hist
     return feature
 
-def generate_feature(data,threshold_WAMP=30,threshold_ZC=0,threshold_SSC=0,bins=9,ranges=(-10,10)):
+def generate_feature(data,threshold_WAMP=30,
+                     threshold_ZC=0,
+                     threshold_SSC=0,
+                     bins=9,
+                     ranges=(-10,10),
+                     show_para=True):
     IEMG = compute_IEMG(data)
     MAV = compute_MAV(data)
     SSI = compute_SSI(data)
@@ -131,12 +136,21 @@ def generate_feature(data,threshold_WAMP=30,threshold_ZC=0,threshold_SSC=0,bins=
     AR = compute_AR(data)
     HIST = compute_HIST(data,bins=bins,ranges=ranges)
     feature = np.concatenate([IEMG,MAV,SSI,VAR,RMS,WL,ZC,SSC,WAMP,skew,Acti,AR,HIST],axis =1)
-    print('threshold_WAMP:%0.1f, threshold_ZC:%0.1f, threshold_SSC:%0.1f,bins:%d,ranges:(%d,%d)'
+    if show_para:
+        print('threshold_WAMP:%0.1f, threshold_ZC:%0.1f, threshold_SSC:%0.1f,bins:%d,ranges:(%d,%d)'
           %(threshold_WAMP,threshold_ZC,threshold_SSC,bins,ranges[0],ranges[1]))
-    print('IEMG,MAV,SSI,VAR,RMS,WL,ZC,SSC,WAMP,skew,Acti,AR,HIST')
+        print('IEMG,MAV,SSI,VAR,RMS,WL,ZC,SSC,WAMP,skew,Acti,AR,HIST')
     return feature
 
-def pipeline_feature(path,width = 256,stride = 32,scaler=False,threshold_WAMP=30,threshold_ZC=0,threshold_SSC=0,bins=9,ranges=(-10,10)):
+def pipeline_feature(path,width = 256,
+                     stride = 32,
+                     scaler=False,
+                     threshold_WAMP=30,
+                     threshold_ZC=0,
+                     threshold_SSC=0,
+                     bins=9,
+                     ranges=(-10,10),
+                     show_para=True):
     emg_data = pd.read_csv(path)
     emg_data = emg_data.fillna({'LEFT_TA':emg_data.LEFT_TA.mean(),
                            'LEFT_TS':emg_data.LEFT_TS.mean(),
@@ -147,5 +161,10 @@ def pipeline_feature(path,width = 256,stride = 32,scaler=False,threshold_WAMP=30
                            'RIGHT_BF':emg_data.RIGHT_BF.mean(),
                            'RIGHT_RF':emg_data.RIGHT_RF.mean()})
     x,y = generate_window_slide_data(emg_data,width=width,stride=stride,scaler=scaler)
-    feature = generate_feature(x,threshold_WAMP=threshold_WAMP,threshold_ZC=threshold_ZC,threshold_SSC=threshold_SSC,bins=bins,ranges=ranges)
+    feature = generate_feature(x,threshold_WAMP=threshold_WAMP,
+                               threshold_ZC=threshold_ZC,
+                               threshold_SSC=threshold_SSC,
+                               bins=bins,
+                               ranges=ranges,
+                               show_para=show_para)
     return feature,y
